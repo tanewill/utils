88235ca2eb36b47ca1bcb78aad237398a6c24c10
Taylor Newill <tanewill@microsoft.com>
2016-04-12 10:38:42 -0400

    Update myClusRun.sh

--- myClusRun.sh
+++ myClusRun.sh
@@ -6,11 +6,12 @@ PASS=Azure@123
 NAMES=`cat nodenames.txt` #names from names.txt file
 for NAME in $NAMES; do
   echo "working on $NAME"
-# copy the file to the remote server and don’t ask to check each fingerprint!
 
-  sshpass -p $PASS ssh $USER@$NAME $1
+#the & here will fork off a run for each node and move to the next, the wait at the end waits until all is complete
+  sshpass -p $PASS ssh $USER@$NAME $1 &
 #  cp /home/$USER/.ssh/id_rsa.pub authorized_keys
 #  sshpass -p $PASS scp -o "StrictHostKeyChecking no" /home/$USER/authorized_keys $USER@$NAME:/home/$USER/.ssh/authorized_keys
 
   echo "completed $NAME"
 done
+wait
