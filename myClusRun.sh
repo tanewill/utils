66be2da4c0c6bca7b6a7d44fa2cc7a5b7c00ad4f
Taylor Newill <tanewill@microsoft.com>
2016-04-13 04:27:22 -0400

    Update myClusRun.sh

--- myClusRun.sh
+++ myClusRun.sh
@@ -4,14 +4,10 @@ USER=azureuser
 PASS=Azure@123
 
 NAMES=`cat nodenames.txt` #names from names.txt file
+echo "launching $1 on $NAMES"
 for NAME in $NAMES; do
-  echo "working on $NAME"
-
-#the & here will fork off a run for each node and move to the next, the wait at the end waits until all is complete
+  #the & here will fork off a run for each node and move to the next, the wait at the end waits until all is complete
   sshpass -p $PASS ssh $USER@$NAME $1 &
-#  cp /home/$USER/.ssh/id_rsa.pub authorized_keys
-#  sshpass -p $PASS scp -o "StrictHostKeyChecking no" /home/$USER/authorized_keys $USER@$NAME:/home/$USER/.ssh/authorized_keys
-
-  echo "completed $NAME"
 done
 wait
+echo "completed $1 for $NAMES"
