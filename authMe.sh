1a9180de6e161e3f380227ff6e8333ece18f434d
Taylor Newill <tanewill@microsoft.com>
2016-04-20 21:25:05 -0400

    Update authMe.sh

--- authMe.sh
+++ authMe.sh
@@ -13,15 +13,15 @@ for NAME in $NAMES; do
       continue
   fi
 
-  sshpass -p $PASS ssh $USER@$NAME 'rm -rf /home/'$USER'/.ssh && mkdir /home/'$USER'/.ssh && chmod 750 /home/'$USER'/.ssh'
-  sshpass -p $PASS scp -o "StrictHostKeyChecking no" /home/$USER/.ssh/authorized_keys $USER@$NAME:/home/$USER/.ssh/authorized_keys
-  sshpass -p $PASS ssh $USER@$NAME 'chmod 644 /home/$USER/.ssh/authorized_keys'
-  sshpass -p $PASS scp -o "StrictHostKeyChecking no" /home/$USER/.ssh/known_hosts $USER@$NAME:/home/$USER/.ssh/known_hosts
-  sshpass -p $PASS scp -o "StrictHostKeyChecking no" /home/$USER/.ssh/id_rsa $USER@$NAME:/home/$USER/.ssh/id_rsa
-  sshpass -p $PASS ssh $USER@$NAME 'touch /home/'$USER'/config'
-  sshpass -p $PASS ssh $USER@$NAME 'echo Host *' >>  /home/$USER/.ssh/config
-  sshpass -p $PASS ssh $USER@$NAME 'echo StrictHostKeyChecking no' >> /home/$USER/.ssh/config
-  sshpass -p $PASS ssh $USER@$NAME 'chmod 400 /home/'$USER'/.ssh/config'
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'rm -rf /home/'$USER'/.ssh && mkdir /home/'$USER'/.ssh && chmod 750 /home/'$USER'/.ssh'
+  sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=1 /home/$USER/.ssh/authorized_keys $USER@$NAME:/home/$USER/.ssh/authorized_keys
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'chmod 644 /home/$USER/.ssh/authorized_keys'
+  sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=2 /home/$USER/.ssh/known_hosts $USER@$NAME:/home/$USER/.ssh/known_hosts
+  sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=2 /home/$USER/.ssh/id_rsa $USER@$NAME:/home/$USER/.ssh/id_rsa
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'touch /home/'$USER'/config'
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'echo Host *' >>  /home/$USER/.ssh/config
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'echo StrictHostKeyChecking no' >> /home/$USER/.ssh/config
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'chmod 400 /home/'$USER'/.ssh/config'
   echo "completed $NAME"
 done
 
