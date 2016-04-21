eddc273fa314f75b64df67a63088efd7dd0f731d
Taylor Newill <tanewill@microsoft.com>
2016-04-21 08:52:38 -0400

    Update authMe.sh

--- authMe.sh
+++ authMe.sh
@@ -11,19 +11,19 @@ cp /home/$USER/.ssh/id_rsa.pub /home/$USER/.ssh/authorized_keys
 NAMES=`cat nodenames.txt` #names from names.txt file
 for NAME in $NAMES; do
   echo "working on $NAME"
-  if [ $NAME = $HOSTIP ]
+  if [ $NAME != $HOSTIP ]
     then
-      echo "$HOSTIP" is current machine
+        echo $NAME is not headnode
+                sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'rm -rf /home/'$USER'/.ssh && mkdir /home/'$USER'/.ssh && chmod 750 /home/'$USER'/.ssh'
+                sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=1 /home/$USER/.ssh/authorized_keys $USER@$NAME:/home/$USER/.ssh/authorized_keys
+                sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=2 /home/$USER/.ssh/known_hosts $USER@$NAME:/home/$USER/.ssh/known_hosts
+                sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=2 /home/$USER/.ssh/id_rsa $USER@$NAME:/home/$USER/.ssh/id_rsa
       continue
   fi
 
-  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'rm -rf /home/'$USER'/.ssh && mkdir /home/'$USER'/.ssh && chmod 750 /home/'$USER'/.ssh'
-  sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=1 /home/$USER/.ssh/authorized_keys $USER@$NAME:/home/$USER/.ssh/authorized_keys
   sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'chmod 644 /home/$USER/.ssh/authorized_keys'
-  sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=2 /home/$USER/.ssh/known_hosts $USER@$NAME:/home/$USER/.ssh/known_hosts
-  sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=2 /home/$USER/.ssh/id_rsa $USER@$NAME:/home/$USER/.ssh/id_rsa
-  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'touch /home/'$USER'/config'
-  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'echo "Host *" >>  /home/'$USER'/.ssh/config'
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'touch /home/'$USER'/.ssh/config'
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'echo "Host *" >  /home/'$USER'/.ssh/config'
   sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'echo StrictHostKeyChecking no >> /home/'$USER'/.ssh/config'
   sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'chmod 400 /home/'$USER'/.ssh/config'
   echo "completed $NAME"
