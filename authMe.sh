2d74b0f6856750525aa4bfb3cb9a52310633f5c9
Taylor Newill <tanewill@microsoft.com>
2016-05-12 08:31:41 -0400

    Update authMe.sh

--- authMe.sh
+++ authMe.sh
@@ -11,15 +11,15 @@ cp /home/$USER/.ssh/id_rsa.pub /home/$USER/.ssh/authorized_keys
 NAMES=`cat nodenames.txt` #names from names.txt file
 for NAME in $NAMES; do
   echo "working on $NAME"
-  if [ $NAME != $HOSTIP ]
-    then
-        echo $NAME is not headnode
+#  if [ $NAME != $HOSTIP ]
+#    then
+#        echo $NAME is not headnode
                 sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'rm -rf /home/'$USER'/.ssh && mkdir /home/'$USER'/.ssh && chmod 750 /home/'$USER'/.ssh'
                 sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=1 /home/$USER/.ssh/authorized_keys $USER@$NAME:/home/$USER/.ssh/authorized_keys
                 sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=2 /home/$USER/.ssh/known_hosts $USER@$NAME:/home/$USER/.ssh/known_hosts
                 sshpass -p $PASS scp -o "StrictHostKeyChecking no" -o ConnectTimeout=2 /home/$USER/.ssh/id_rsa $USER@$NAME:/home/$USER/.ssh/id_rsa
-      continue
-  fi
+#      continue
+#  fi
 
   sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'chmod 644 /home/$USER/.ssh/authorized_keys'
   sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'touch /home/'$USER'/.ssh/config'
