4488a9bd64e61a9a26a7b2866cd09e7d8b22e463
Taylor Newill <tanewill@microsoft.com>
2016-06-10 07:22:35 -0400

    Update myClusRun.sh

--- myClusRun.sh
+++ myClusRun.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-USER=azureuser
-PASS=Azure@123
+USER=$1
+PASS=$2
 
 NAMES=`cat nodenames.txt` #names from names.txt file
 echo "launching $1"
 for NAME in $NAMES; do
   #the & here will fork off a run for each node and move to the next, the wait at the end waits until all is complete
-  sshpass -p $PASS ssh $USER@$NAME $1 &
+  sshpass -p $PASS ssh $USER@$NAME $3 &
 done
 wait
-echo "completed $1"
+echo "completed $3"
