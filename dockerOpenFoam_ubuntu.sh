5a42f30f5af63a664e97477f7f70d70e65d959bd
Taylor Newill <tanewill@microsoft.com>
2016-07-04 16:49:15 -0400

    Update dockerOpenFoam_ubuntu.sh

--- dockerOpenFoam_ubuntu.sh
+++ dockerOpenFoam_ubuntu.sh
@@ -2,6 +2,7 @@
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+touch /etc/apt/sources.list.d/docker.list
 sudo echo "deb https://apt.dockerproject.org/repo ubuntu-precise main" >> /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-get purge lxc-docker
